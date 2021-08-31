3c49b696284f45678b43ba23dbe9f77da45169a7
audryf <84256033+audryf@users.noreply.github.com>
2021-08-31 13:13:29 -0500

    update

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mysql = require('mysql2');
+const { getMaxListeners } = require('process');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +8,15 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Charlie19$',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((req, res) => {
     res.status(404).end();
